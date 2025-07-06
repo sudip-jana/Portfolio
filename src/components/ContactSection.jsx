7dfef552173196ee46d40763cf034607ae98f60b
Sudip <sudipjana0702@gmail.com>
2025-07-06 13:27:37 +0530

    added linkedin

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -93,7 +93,7 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/sudipjana07" target="_blank">
                   <Linkedin />
                 </a>
                 <a href="https://x.com/Sudip21779431?t=lrDvUsINxjifClvRczhPlA&s=09" target="_blank">
